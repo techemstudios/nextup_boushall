09f8995a7446511d79f1de58427802d82e13d662
joetechem <joe@techemstudios.com>
2017-10-09 11:00:34 -0400

    adding comments to hello world pygame

--- README.md
+++ README.md
@@ -39,3 +39,31 @@ Pygame is part of the Python framework including tools to create video games. Wh
 #### Initialization  
 
 #### The Main Game Loop  
+```python    
+# importing dependencies
+import pygame, sys
+from pygame.locals import *
+
+# initialize
+pygame.init()
+
+# window width, height --> units = pixels
+screen = pygame.display.set_mode((400, 300))
+
+# window caption
+pygame.display.set_caption('Hello World!')
+
+# the main game loop
+while True:
+    sys_font = pygame.font.SysFont("None", 19)
+    rendered = sys_font.render('Hello World', 0, (255, 100, 100))
+    # the blit() method, draws items to the surface
+    screen.blit(rendered, (100, 100))
+
+    # the quit event
+    for event in pygame.event.get():
+        if event.type == QUIT:
+            pygame.quit()
+            sys.exit()
+    pygame.display.update()
+```  
--- pygame_code/HelloWorld/hello_world_main.py
+++ pygame_code/HelloWorld/hello_world_main.py
@@ -13,9 +13,11 @@ import pygame, sys
 from pygame.locals import *
 
 # initialize
+# Must be called before any other pygame functions
 pygame.init()
 
-# window width, height --> units = pixels
+# Creates the Surface object
+# window width, height as a tuple --> units = pixels
 screen = pygame.display.set_mode((400, 300))
 
 # window caption
@@ -23,13 +25,16 @@ pygame.display.set_caption('Hello World!')
 
 # the main game loop
 while True:
-    sys_font = pygame.font.SysFont("None", 19)
-    rendered = sys_font.render('Hello World', 0, (255, 100, 100))
+    sys_font = pygame.font.SysFont("None", 24)
+    # Draws text to surface
+    rendered = sys_font.render('Hello World!', 0, (255, 100, 100))
+    # Draws on the surface
     screen.blit(rendered, (100, 100))
 
     # the quit event
-    for event in pygame.event.get():
+    for event in pygame.event.get():    # gets a list of Event objects
         if event.type == QUIT:
             pygame.quit()
             sys.exit()
+    # refreshes the surface
     pygame.display.update()
