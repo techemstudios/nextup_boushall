3016017573bb359bd004f038ec7ff3b485d43150
joetechem <joe@techemstudios.com>
2017-10-09 12:37:32 -0400

    artifical intelli with pygame

--- pygame_code/artificial_intelligence/clustering.py
+++ pygame_code/artificial_intelligence/clustering.py
@@ -1,23 +1,23 @@
 import numpy
-import skleanr.cluster
+import sklearn.cluster
 import pygame, sys
 from pygame.locals import *
 
 positions = numpy.random.randint(0, 400, size =(30, 2))
 
-positions_norms = numpy.sum(positions ** 2, axis=1)
+positions_norms = numpy.sum(positions ** 2, axis = 1)
 S = - positions_norms[:, numpy.newaxis] - positions_norms[numpy.newaxis, :] + 2 * numpy.dot(positions, positions.T)
 
-aff_pro = sklearn.cluster.AffinityPropagation().fir(S)
+aff_pro = sklearn.cluster.AffinityPropagation().fit(S)
 labels = aff_pro.labels_
 
 polygon_points = []
 
 for i in xrange(max(labels) + 1):
-    polygon_points.appen([])
+    polygon_points.append([])
 
 # Sorting each point by cluster
-for i, 1 in enumerate(lables):
+for i, l in enumerate(labels):
     polygon_points[1].append(positions[i])
 
 pygame.init()
@@ -30,6 +30,7 @@ while True:
     for event in pygame.event.get():
         if event.type == QUIT:
             pygame.quit()
+
             sys.exit()
 
     pygame.display.update()
